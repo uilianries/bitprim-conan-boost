9cda06f8106cfec1239fd943a1850cabaa8a8ab4
Dario Ramos <dario.fenix@gmail.com>
2017-08-02 22:51:31 -0300

    DEV-8: Use new Bintry repo for all packages

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,7 +22,6 @@ fi
 pip install conan --upgrade
 pip install conan_package_tools
 
-conan remote add conan_bzip2 https://api.bintray.com/conan/bitprim/bitprim-conan-bzip2
-conan remote add conan_zlib https://api.bintray.com/conan/bitprim/bitprim-conan-zlib
+conan remote add conan_bzip2 https://api.bintray.com/conan/bitprim/bitprim
 
 conan user
