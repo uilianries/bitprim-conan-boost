dcc1e6a2a01cbe8a12c176aa6ebb2e71875f88f3
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-17 17:16:43 -0300

    test std lib ABI

--- conanfile.py
+++ conanfile.py
@@ -236,6 +236,17 @@ class BitprimconanboostConan(ConanFile):
 
             #flags.append("define=_GLIBCXX_USE_CXX11_ABI=1")
 
+
+
+            cxx_flags.append("-std=c++11")  # always C++11 (at minimum)
+ 
+            if self.settings.compiler == "gcc":
+                if float(self.settings.compiler.version) >= 5):
+                    flags.append("define=_GLIBCXX_USE_CXX11_ABI=1")
+                else:
+                    flags.append("define=_GLIBCXX_USE_CXX11_ABI=0")
+
+
             if "clang" in str(self.settings.compiler):
                 if str(self.settings.compiler.libcxx) == "libc++":
                     cxx_flags.append("-stdlib=libc++")
