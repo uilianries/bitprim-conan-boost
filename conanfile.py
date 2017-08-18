c032929df1d394fe3572ebaf020d4fa2066d625a
dramos <dario.fenix@gmail.com>
2017-08-17 23:13:50 -0300

    DEV-10: Test

--- conanfile.py
+++ conanfile.py
@@ -243,8 +243,10 @@ class BitprimconanboostConan(ConanFile):
             if self.settings.compiler == "gcc":
                 if float(self.settings.compiler.version) >= 5:
                     flags.append("define=_GLIBCXX_USE_CXX11_ABI=1")
+                    cxx_flags.append("_GLIBCXX_USE_CXX11_ABI=1")
                 else:
                     flags.append("define=_GLIBCXX_USE_CXX11_ABI=0")
+                    cxx_flags.append("_GLIBCXX_USE_CXX11_ABI=0")
 
             if "clang" in str(self.settings.compiler):
                 if str(self.settings.compiler.libcxx) == "libc++":
