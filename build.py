1f65248353344c0d86e444d8d08cb911bd48a865
dramos <dario.fenix@gmail.com>
2017-08-18 02:02:06 -0300

    DEV-10: Test

--- build.py
+++ build.py
@@ -3,23 +3,25 @@ import os
 import platform
 
 if __name__ == "__main__":
-    #TODO Specify combinations here using archs argument
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs="x86_64")
     builder.add_common_builds(shared_option_name="bitprim-conan-boost:shared", pure_c=False)
 
     filtered_builds = []
+    add_special_case = False
     for settings, options, env_vars, build_requires in builder.builds:
-        # filtered_builds.append([settings, options, env_vars, build_requires])
         print(settings)
         print(options)
         if settings["build_type"] == "Release" \
                 and options["bitprim-conan-boost:shared"] == False:
             #TODO: Adding gcc 4.9 c++11 build manually until Conan fixes it
             if settings["compiler"] == "gcc" and settings["compiler.version"] == "4.9":
-                builder.add(settings={'compiler.version': '4.9', 'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'}, options={'bitprim-conan-boost:shared': False})
+                add_special_case = True
             else:
                 if not "compiler.libcxx" in settings or settings["compiler"] == "apple-clang" or ("compiler.libcxx" in settings and settings["compiler.libcxx"] == "libstdc++"):
                     filtered_builds.append([settings, options, env_vars, build_requires])
+
+    if add_special_case:
+        builder.add(settings={'compiler.version': '4.9', 'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc'}, options={'bitprim-conan-boost:shared': False})
 
     builder.builds = filtered_builds
     builder.run()
