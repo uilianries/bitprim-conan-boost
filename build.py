8b7df0ccbf12452388829deeca77c166346fc617
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-18 23:55:01 -0300

    fixes

--- build.py
+++ build.py
@@ -2,14 +2,12 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
-    builder.add_common_builds(shared_option_name="bitprim-conan-boost:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="bitprim-conan-boost:shared")
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        # print(settings)
-        # print(options)
         if settings["build_type"] == "Release" \
-                and options["bitprim-conan-boost:shared"] == False:
+                and not options["bitprim-conan-boost:shared"]:
 
             filtered_builds.append([settings, options, env_vars, build_requires])
 
