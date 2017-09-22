4447215c289ddea9f62c9bb65b552fa0ad0be4bf
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-09-22 18:39:03 -0300

    Debug and Release builds

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if settings["build_type"] == "Release" \
+        if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
                 and not options["bitprim-conan-boost:shared"]:
 
             filtered_builds.append([settings, options, env_vars, build_requires])
